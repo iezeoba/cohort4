e3f2025eefd3d0910126b9bda074a76a86166fa3
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-02-24 20:59:38 +0100

    updated functions file

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -4,7 +4,14 @@ const functions = {
     size: (num) => {
         if (num < 10) return "small";
         if (num < 20) return "medium";
-        return "large";
+        if (num <= 100) return "large";
+        return "extra large";
+
+    },
+
+    isEven: (num)=> {
+        if (num%2 == 0) return "true";
+        else return "false";
     },
 
     add: (num1, num2) => {
--- 01-getting-started/src/scripts/functions.test.js
+++ 01-getting-started/src/scripts/functions.test.js
@@ -5,11 +5,16 @@ test('Check the sizes', () => {
     expect(functions.size(0)).toBe("small");
     expect(functions.size(10)).toBe("medium");
     expect(functions.size(15)).toBe("medium");
-    expect(functions.size(20)).toBe("large");
-    expect(functions.size(2000000)).toBe("large");
+    expect(functions.size(20)).toBe("large");    
+    expect(functions.size(101)).toBe("extra large");
 });
 
 test('Does that add function work?', () => {
     expect(functions.add(1,2)).toBe(3);
     expect(functions.add(101,202)).toBe(303);
+});
+
+test ("is the number even?", () => {
+    expect(functions.isEven(2)).toBe("true");
+    expect(functions.isEven(3)).toBe("false");
 });
