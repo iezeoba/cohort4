1ab2fcf757ed0abf17653a484e2d63cf5c931e90
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-03-17 12:07:57 -0600

    re-uploading 02-dom

---
+++ 02-dom/src/scripts/dom.js
@@ -0,0 +1,54 @@
+// Basic DOM Section
+// idMainDiv.addEventListener("click", () => {
+//     idMainDiv.style.backgroundColor = "#add8e6";
+//     idMainDiv.style.backgroundColor = "#ffffff";
+//     idStatusBox.textContent = "This is the div";
+//     console.log("This is the div");
+// });
+// idH1element.addEventListener("click", () => {
+//     idStatusBox.textContent = "This is the h1 element - Header";
+//     console.log("This is the h1 element - Header")
+// });
+// idParagraph.addEventListener("click", () => {
+//     idStatusBox.textContent = "This is the paragraph element";
+//     console.log("This is the paragraph element")
+// });
+// idOrderList.addEventListener("click", () => {
+//     idStatusBox.textContent = "This is the ordered list element";
+//     console.log("This is the ordered list element")
+// });
+document.body.addEventListener("click", (e) => {
+    console.log("This is the", e.target);
+    idStatusBox.textContent = "My message" + " " + idInputItem.value;
+});
+idShowItem.addEventListener("click", () => {
+    let showList = document.getElementsByTagName("ol")[0];
+    console.log(showList);
+});
+idAddItem.addEventListener("click", () => {
+    if (idInputItem.value.length > 0) {
+    let newLi = document.createElement("li");
+    let newItem = document.createTextNode(idInputItem.value);
+    newLi.appendChild(newItem);
+    document.getElementById("idOrderList").appendChild(newLi);
+    idInputItem.value = "";
+    }
+});
+document.querySelector("#idInputItem").addEventListener("keypress", function (e) {
+    if (idInputItem.value.length > 0 && e.key === "Enter") {
+    let newLi = document.createElement("li");
+    let newItem = document.createTextNode(idInputItem.value);
+    newLi.appendChild(newItem);
+    var button = document.createElement("button");
+    button.textContent = "Remove";
+    newLi.appendChild(button);
+    newLi.setAttribute("id","element4");
+    newItem.appendChild(newLi);
+    console.log(newLi.id);
+    document.getElementById("idOrderList").appendChild(newLi);
+    idInputItem.value = "";
+    }
+});
+
+// Working With Cards
+
