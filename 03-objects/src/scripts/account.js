84c058b954f7dfd93e552cc8e374bebd2bdc9381
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-04-01 00:53:11 -0600

    updating code files for {account}

--- 03-objects/src/scripts/account.js
+++ 03-objects/src/scripts/account.js
@@ -3,49 +3,58 @@ export class Account {
         this.name = name;
         this.balance = balance;
     }
-    deposit(amount) {
-        if (!this._isPositive(amount)) {
-            // console.log(`Deposit: ${this.name} new balance is ${this.balance}`);
-            return "Amount entered is negative";
-        }   else return this.balance += amount;
+    deposit(amt) {
+        let amount = parseFloat(amt);
+    if (!this._isPositive(amount)) {
+        // console.log(`Deposit: ${this.name} new balance is ${this.balance}`);
+        return "Amount entered is negative";
+    } else return (this.balance += amount).toFixed(2);
 }
-    withdraw(amount) {
-        if (this._isAllowed(amount)) {
-            this.balance -= amount;
-            // console.log(`Withdraw: ${this.name} new balance is ${this.balance}`);
-            return this.balance;
-        } else if (amount < 0) {
-            return "Amount is negative"
-        } else if (!this._isAllowed(amount)) { 
-            return "Insufficient balance";
-    }
-}
-    transfer() {
-        if (this.withdraw(amount) && account.deposit(amount)) {
-            console.log(`Transfer: ${amount} has been moved from ${this.name} to ${account.name}`);
-            return true;
-          }
-          return false;
-    }
-    _isPositive(amount) {
-            if (amount < 0) {
-            return false;
-          }
-          return true;
-    }
-    _isAllowed(amount) {
-        if (this._isPositive(amount) && this.balance - amount >= 0) {
-            return true;
-        } else return false;
-    //         const isAllowed = this.balance - amount >= 0;
-    //         if (!isAllowed) {
-    //         console.error('You have insufficent funds!');
-    //         return false;
-    //       }
-    //       return true;
-    // }
+withdraw(amt) {
+    let amount = parseFloat(amt);
+    if (this._isAllowed(amount)) {
+        this.balance -= amount;
+        // console.log(`Withdraw: ${this.name} new balance is ${this.balance}`);
+        return (this.balance).toFixed(2);
+    } else if (amount < 0) {
+        return "Amount is negative"
+    } else if (!this._isAllowed(amount)) {
+        return "Insufficient balance";
     }
 }
+transfer() {
+    if (this.withdraw(amount) && account.deposit(amount)) {
+        console.log(`Transfer: ${amount} has been moved from ${this.name} to ${account.name}`);
+        return true;
+    }
+    return false;
+}
+_isPositive(amount) {
+    if (amount < 0) {
+        return false;
+    }
+    return true;
+}
+_isAllowed(amount) {
+    if (this._isPositive(amount) && this.balance - amount >= 0) {
+        return true;
+    } else return false;
+}
+}
+// export class AccountController {
+//     constructor(name) {
+//         this.name = name;
+//     }
+    // Add methods
+// }
+// const account1 = new Account("Ifeanyi Ezeoba", 100);
+// export class AccountController {
+//     constructor() {
+//         this.bankaccounts = [];
+//     }
+// }
+// // export {Account, AccountController};
+
 // const account1 = new Account ("Ifeanyi Ezeoba", 100);
 // console.log(account1);
 
--- 03-objects/src/scripts/index.js
+++ 03-objects/src/scripts/index.js
@@ -1,84 +1,95 @@
-import {Account} from "./account.js"
+import { Account } from "./account.js"
 
-const myAcount = new Account (acctName.value, 0);
-const newUser = {};
+const myAccount = new Account(acctName.value, 0);
+let newUser = {};
 addAcc.addEventListener("click", newAccount);
 
 function newAccount() {
-    // console.log("testing new acct");
-    newUser.name = acctName.value;
-    if (dropdown.value == "Checking Account") {
-        newUser.CheckingAccount == 0;
-    }
-    else if (dropdown.value == "Savings Account") {
-        newUser.SavingsAccount == 0;
-    }
-    else if (dropdown.value == "Credit Card") {
-        newUser.CreditCard == 0;
-    }
-    else if (dropdown.value == "RESP Account") {
-        newUser.RESPAccount == 0;
+    console.log("testing new acct");
+    console.log(Object.keys(newUser));
+    // ---------- USER DOES NOT EXIST AND HAS NO ACCOUNT ----------
+    if (Object.keys(newUser).includes("name") == false) {
+        if (dropdown.value == "Checking Account") {
+            newUser.name = acctName.value;
+            newUser.CheckingAccount = 0;
+        } else if (dropdown.value == "Savings Account") {
+            newUser.name = acctName.value;
+            newUser.SavingsAccount = 0;
+        } else if (dropdown.value == "Credit Card") {
+            newUser.name = acctName.value;
+            newUser.CreditCard = 0;
+        } else if (dropdown.value == "RESP Account") {
+            newUser.name = acctName.value;
+            newUser.RESPAccount = 0;
+        } else {
+            newUser.name = acctName.value;
+            newUser[`${dropdown.lastChild.value}`] = 0; // newUser
+        }
+        // ---------- USER EXISTS AND HAS AN ACCOUNT ---------- 
+    } else if (Object.keys(newUser).includes("name") && (newUser.name == acctName.value)) {
+        if ((Object.keys(newUser).includes("CheckingAccount") == true) && (dropdown.value == "Checking Account")) {
+            alert("User account already exists");
+        } else if ((Object.keys(newUser).includes("CheckingAccount") == false) && (dropdown.value == "Checking Account")) {
+            newUser.CheckingAccount = 0;
+        }
+        if ((Object.keys(newUser).includes("SavingsAccount") == true) && (dropdown.value == "Savings Account")) {
+            alert("User account already exists");
+        } else if ((Object.keys(newUser).includes("SavingsAccount") == false) && (dropdown.value == "Savings Account")) {
+            newUser.SavingsAccount = 0;
+        }
+        if ((Object.keys(newUser).includes("CreditCard") == true) && (dropdown.value == "Credit Card")) {
+            alert("User account already exists");
+        } else if ((Object.keys(newUser).includes("CreditCard") == false) && (dropdown.value == "Credit Card")) {
+            newUser.CreditCard = 0;
+        }
+        if ((Object.keys(newUser).includes("RESPAccount") == true) && (dropdown.value == "RESP Account")) {
+            alert("User account already exists");
+        } else if ((Object.keys(newUser).includes("RESPAccount") == false) && (dropdown.value == "RESP Account")) {
+            newUser.RESPAccount = 0;
+        }
     }
     updateDisplay();
-
 }
 
 function updateDisplay() {
-    if(Object.keys(newUser).includes("CheckingAccount")) {
-        checkingAcc.textContent = newUser.CheckingAccount;
+    if (Object.keys(newUser).includes("CheckingAccount")) {
+        checkingAcc.value = newUser.CheckingAccount;
     }
-    else if(Object.keys(newUser).includes("SavingsAccount")) {
-    savingsAcc.textContent = newUser.SavingsAccount;
+    else if (Object.keys(newUser).includes("SavingsAccount")) {
+        savingsAcc.value = newUser.SavingsAccount;
     }
-    else if(Object.keys(newUser).includes("CreditCard")) {
-    creditCard.textContent = newUser.creditCard;
+    else if (Object.keys(newUser).includes("CreditCard")) {
+        creditCard.value = newUser.CreditCard;
     }
-    else if(Object.keys(newUser).includes("RESPAccount")) {
-    respAcc.textContent = newUser.RESPAccount;
+    else if (Object.keys(newUser).includes("RESPAccount")) {
+        respAcc.value = newUser.RESPAccount;
     }
 }
 
-// const acctFunctions = {
-//     play() {
-//         console.log("in mortFunctions");
-//     },
-//     // 
-//     // The stub is the same as the signature
-//     // 
-//     calcLoan(principal, yearInterest, years) {
+document.getElementById("addCustAcc").addEventListener("click", insertValue);
 
-//         const interest = yearInterest / 100 /12;
-//         const payments = years * 12;
+function insertValue() {
+    if (idCustAcc.value.length > 0) {
+        let txtAccType = document.getElementById("idCustAcc").value;
+        let newOption = document.createElement("OPTION");
+        let newOptionVal = document.createTextNode(txtAccType);
 
-//     // Now compute the monthly payment figure using esoteric math
-//         const x = Math.pow(1 + interest, payments);
-//         const monthly = (principal * x * interest) / (x - 1);
-//         const total = monthly * payments;
-//         const totalInterest = total - principal;
+        newOption.appendChild(newOptionVal);
+        dropdown.appendChild(newOption);
+        idCustAcc.value = "";
+        // newUser = Object.assign(txtAccType,newUser); // object key
+    }
+}
+idDeposit.addEventListener("click", depositCash);
 
-//     // If we have valid numbers return them
-
-//         if(monthly & total) {
-//             return {
-//                 monthly: mortFunctions.round(monthly),
-//                 total: mortFunctions.round(total),
-//                 interest: mortFunctions.round(totalInterest)
-//             }
-//         }
-    
-//     // If the numbers are not valid 
-
-//             return {
-//                 monthly: "",
-//                 total: "",
-//                 interest: ""
-//             }
-
-//         // return {monthly: 0, total: 0, interest: 0}
-//     },  // return {monthly: 83.79, total: 1005.42, interest: 5.42}
-
-//     round(num) {
-//         return Math.round(num*100) / 100;
-//     }
-// }
-// export default acctFunctions;
+function depositCash(amount) {
+    if (dropdownTrxn.value == "Checking Account") {
+        checkingAcc.value = myAccount.deposit(idAmount.value);
+    } else if (dropdownTrxn.value == "Savings Account") {
+        savingsAcc.value = myAccount.deposit(idAmount.value);
+    } else if (dropdownTrxn.value == "Credit Card") {
+        creditCard.value = myAccount.deposit(idAmount.value);
+    } else if (dropdownTrxn.value == "RESP Account") {
+        respAcc.value = myAccount.deposit(idAmount.value);
+    }
+};
