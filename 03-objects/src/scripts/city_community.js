fb032995ee33c2a2bad80ddf2d7e7b045b7b73a6
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-04-18 23:43:39 -0600

    updating city_community code files

--- 03-objects/src/scripts/city_community.js
+++ 03-objects/src/scripts/city_community.js
@@ -1,12 +1,50 @@
+export class City {
+    constructor() {
+        this.allCities = [];
+        this.count = 0;
+        // this.key = key;
+    }
+
+    show(cityname) {
+        let string = "";
+        return string += `${this.name} is located at latitude ${this.latitude} and longitude ${this.longitude} and has a population of about ${this.population}`;
+    }
+
+    movedIn(immigrants) {
+        this.population = this.population + immigrants;
+    }
+
+    movedOut(exits) {
+        this.population = this.population - exits;
+    }
+
+    howBig() {
+        if (this.population > 100000) {
+            return "City";
+        } else if (this.population > 19999 && this.population <= 100000) {
+            return "Large Town";
+        } else if (this.population > 999 && this.population <= 19999) {
+            return "Town";
+        } else if (this.population > 100 && this.population <= 999) {
+            return "Village";
+        } else {
+            return "Hamlet";
+        }
+    }
+}
+
 export class Community {
     constructor() {
-        this.url = "http://127.0.0.1:5000/";
+        this.newCity = new City();
+        this.cityKey;
+        this.url = 'http://localhost:5000/';
+        // this.url = "http://127.0.0.1:5000/";
         this.allCities = [];
         this.counter = 1;
     }
 
     nextKey() {
-        return `k${this.counter++}`;
+        return this.counter++;
     }
 
     async getCities(url = '', data = {}) {
@@ -19,18 +57,29 @@ export class Community {
 
     async createCity(name, latitude, longitude, population) {
         let key = this.nextKey();
-        this.allCities.push(name, latitude, longitude, population, key);
-        let data = await functions.postData(this.url + "add", { name: name, latitude: latitude, longitude: longitude, population: population, key: key });
-        return data;
+        let city = {};
+        city.name = name;
+        city.latitude = latitude;
+        city.longitude = longitude;
+        city.population = population;
+        city.key = key;
+        this.allCities.push(city);
+        // let data = await functions.postData(this.url + "add", { name: name, latitude: latitude, longitude: longitude, population: population, key: key });
+        return city;
     }
 
-    cityFinder(local) {
-        let index = this.allCities.findIndex(city => city.name === local);
+    cityFinder(cityname) {
+        let index;// = this.allCities.findIndex(city => city.name === cityname);
+        for (let i = 0; i < this.allCities.length; i++) {
+            if (cityname === this.allCities[i].name) {
+                    index = i
+            }
+        }
         return this.allCities[index];
     }
 
-    whichSphere(local) {
-        let sphere = this.cityFinder(local).latitude;
+    whichSphere(cityname) {
+        let sphere = this.cityFinder(cityname).latitude;
         if (sphere > 0) {
             return `This location is in the Northern Hemisphere`;
         } else if (sphere < 0) {
@@ -39,7 +88,7 @@ export class Community {
             return "This location is on the equator";
         }
     }
-    getMostNorthern() {
+    getMostNorthern(this.allCities) {
         let string = "";
         return string += `${Object.values(this.allCities.reduce((a, b) => b.latitude > a.latitude ? b : a))}`;
     }
@@ -56,59 +105,18 @@ export class Community {
         } return sum;
     }
 
-    increasePopulation(local, amount) {
-        this.cityFinder(local).movedIn(amount);
-        return this.cityFinder(local).currentPopulation();
+    increasePopulation(cityname, amount) {
+        this.cityFinder(cityname).movedIn(amount);
+        return this.cityFinder(cityname).this.population();
     }
 
-    decreasePopulation(local, amount) {
+    decreasePopulation(cityname, amount) {
         this.cityFinder(local).movedOut(amount);
-        return this.cityFinder(local).currentPopulation();
+        return this.cityFinder(cityname).this.population();
     }
 
-    deleteCity(local) {
-        this.allCities.splice(this.cityFinder(local), 1);
+    deleteCity(cityname) {
+        this.allCities.splice(this.cityFinder(cityname), 1);
     }
 };
-
-export class City {
-    constructor(name, latitude, longitude, population, key) {
-        this.name = name;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.population = population;
-        this.key = key;
-    }
-
-    show() {
-        let string = "";
-        return string += `${this.name} is located at latitude ${this.latitude} and longitude ${this.longitude} and has a population of about ${this.population}`;
-    }
-
-    movedIn(immigrants) {
-        this.population = this.population + immigrants;
-    }
-
-    movedOut(exits) {
-        this.population = this.population - exits;
-    }
-
-    currentPopulation() {
-        return this.population;
-    }
-
-    howBig() {
-        if (this.population > 100000) {
-            return "City";
-        } else if (this.population > 19999 && this.population <= 100000) {
-            return "Large Town";
-        } else if (this.population > 999 && this.population <= 19999) {
-            return "Town";
-        } else if (this.population > 100 && this.population <= 999) {
-            return "Village";
-        } else {
-            return "Hamlet";
-        }
-    }
     // export default {Community, City}; //this collective export method is an alternative to the private export method used in this file. both are valid.
-};
--- 03-objects/src/scripts/city_community.test.js
+++ 03-objects/src/scripts/city_community.test.js
@@ -1,83 +1,54 @@
 import { City, Community } from './city_community.js';
 // import { Community } from './city_community.js';
 
-
-let ct = new Community();
-ct.createCity('Airdrie', 51.2927, 114.0134, 700);
-ct.createCity('Calgary', 51.0447, 548.2548, 189000);
-ct.createCity('British Columbia', 11.2927, 554.2548, 25000);
-
-test("Testing createCity() in Community Class", () => {
-    let myCity = new Community();
-    myCity.createCity("Halifax", 45.3658, 52.3698, 985000);
-    expect(myCity.newCt.allCities.length).toBe(1);
-    let myFav = myCity;
-    expect(myCity.newCt.allCities[0].population).toBe(985000);
-    expect(myFav.newCt.allCities[0].population).toBe(985000);
-    myCity.newCt.allCities[0].population = myCity.newCt.allCities[0].population + 5000;
-    expect(myCity.newCt.allCities[0].population).toBe(990000);
-    expect(myFav.newCt.allCities[0].population).toBe(990000);
-    myFav.newCt.allCities[0].population = myFav.newCt.allCities[0].population - 10000;
-    expect(myCity.newCt.allCities[0].population).toBe(980000);
-    expect(myFav.newCt.allCities[0].population).toBe(980000);
-
-    console.log(myCity.newCt.allCities[0].population);
-    console.log(myFav.newCt.allCities[0].population);
-
-
+test("Testing nextKey", () => {
+let city = new Community();
+expect(city.nextKey()).toBe(1);
 });
 
-test("Testing Show() in City Class", () => {
-    expect(ct.newCt.show('Airdrie')).toBe('Airdrie is at latitude:51.2927 and longitude:114.0134 with a population of 700');
-    expect(ct.newCt.show('Calgary')).toBe('Calgary is at latitude:51.0447 and longitude:548.2548 with a population of 189000');
-    expect(ct.newCt.show('British Columbia')).toBe('British Columbia is at latitude:11.2927 and longitude:554.2548 with a population of 25000');
-    expect(ct.newCt.allCities.length).toBe(3);
+test("Testing createCity", () => {
+    let city = new Community();
+city.createCity('Lethbridge', 52.92, 79.04, 700);
+expect(city.allCities.length).toBe(1);
+city.createCity('Calgary', 53.07, 62.12, 189000);
+city.createCity('Medicine Hat', 41.92, 56.25, 2500);
+console.log(city.allCities);
+expect(city.allCities.length).toBe(3);
 });
 
-test("Testing movedIn() in City Class", () => {
-    expect(ct.newCt.movedIn('Airdrie', 10)).toBe(710);
-    expect(ct.newCt.movedIn('Calgary', 1000)).toBe(190000);
-    expect(ct.newCt.movedIn('British Columbia', 5000)).toBe(30000);
+test("Testing cityFinder", () => {
+let city = new Community();
+city.createCity('Lethbridge', 52.92, 79.04, 700);
+city.createCity('Calgary', 53.07, 62.12, 189000);
+city.createCity('Medicine Hat', 41.92, 56.25, 2500);
+expect(city.cityFinder("Calgary").latitude).toBe(53.07);
+expect(city.cityFinder("Medicine Hat").longitude).toBe(56.25);
+expect(city.cityFinder("Lethbridge").population).toBe(700);
+});
 
-});
-test("Testing movedOut() in City Class", () => {
-    expect(ct.newCt.movedOut('Airdrie', 20)).toBe(690);
-    expect(ct.newCt.movedOut('Calgary', 10000)).toBe(180000);
-    expect(ct.newCt.movedOut('British Columbia', 15000)).toBe(15000);
+test("Testing whichSphere", () => {
+    let city = new Community();
+    city.createCity('Lethbridge', 52.92, 79.04, 700);
+    city.createCity('Calgary', 53.07, 62.12, 189000);
+    city.createCity('Medicine Hat', 41.92, 56.25, 2500);
+    expect(city.whichSphere("Calgary")).toBe("This location is in the Northern Hemisphere");
+    expect(city.whichSphere("Medicine Hat")).toBe("This location is in the Northern Hemisphere");
+    expect(city.whichSphere("Lethbridge")).toBe("This location is in the Northern Hemisphere");
+    });
 
-});
-test("Testing howBig() in City Class", () => {
-    expect(ct.newCt.howBig('Airdrie')).toBe("Village");
-    expect(ct.newCt.howBig('Calgary')).toBe("City");
-    expect(ct.newCt.howBig('British Columbia')).toBe("Town");
+    test("Testing whichSphere", () => {
+        let city = new Community();
+        let cityArr = [];
+        cityArr.push(city.createCity('Lethbridge', 52.92, 79.04, 700));
+        cityArr.push(city.createCity('Calgary', 53.07, 62.12, 189000));
+        cityArr.push(city.createCity('Medicine Hat', 41.92, 56.25, 2500));
+        // expect(city.whichSphere("Calgary")).toBe("This location is in the Northern Hemisphere");
+        // expect(city.whichSphere("Medicine Hat")).toBe("This location is in the Northern Hemisphere");
+        expect(city.getMostNorthern(cityArr)).toBe("Calgary");
+        });
 
-});
-test("Testing whichSphere() in Community Class", () => {
-    expect(ct.whichSphere('Airdrie')).toBe("Northern Hemisphere");
-    expect(ct.whichSphere('Calgary')).toBe("Northern Hemisphere");
-    expect(ct.whichSphere('British Columbia')).toBe("Southern Hemisphere");
 
-});
-test("Testing getMostNorthern() &  getMostSouthern() in Community Class", () => {
-    expect(ct.getMostNorthern()[0]).toBe("Airdrie");
-    expect(ct.getMostNorthern()[1]).toBe(51.2927);
-    expect(ct.getMostSouthern()[0]).toBe("British Columbia");
-    expect(ct.getMostSouthern()[1]).toBe(11.2927);
 
-});
-test("Testing getTotalPopulation() in Community Class", () => {
-    expect(ct.getTotalPopulation()).toBe(195690);
 
-});
-test("Testing getPopulationofCity() in Community Class", () => {
-    expect(ct.getPopulationofCity('Airdrie')).toBe(690);
-    expect(ct.getPopulationofCity('Calgary')).toBe(180000);
-    expect(ct.getPopulationofCity('British Columbia')).toBe(15000);
 
-});
-test("Testing deleteCity() in Community Class", () => {
-    expect(ct.newCt.allCities.length).toBe(3);
-    ct.deleteCity('Calgary');
-    ct.deleteCity('British Columbia');
-    expect(ct.newCt.allCities.length).toBe(1);
-});
+
