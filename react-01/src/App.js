bab51eaf6bb734346a250bbbdbcdca727bf5c9a4
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-05-07 23:04:23 -0600

    building App.js components

--- react-01/src/App.js
+++ react-01/src/App.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 //function App() {
 class App extends React.Component {  
   constructor() { 
     super();
     this.counter = 21;
+    this.state = {
+      myState: "TBD"
+    };
 }
-
+onPushMe = () => {
+  console.log("You pushed me");
+  this.counter++;
+  console.log(this.counter);
+  this.setState({
+    myState: "now:" + this.counter
+  });
+}
 render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World</h1>
-        <h2>I am in control of this application and my name is Ifeanyi {this.counter}</h2>
+        <h2>I am in control of this application and my name is Ifeanyi {this.state.myState}</h2>
         <button onClick={this.onPushMe}>Push Me</button>
+        <MyComponent />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
