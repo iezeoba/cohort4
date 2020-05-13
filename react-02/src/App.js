db0ee4fe5dce83ab68d24959440bd05c8298275e
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-05-13 02:20:36 -0600

    setting svg images

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import dolphin from './svg/dolphin.svg';
+import menu from './svg/menu.svg';
+import lego from './svg/lego.svg';
+import maps from './svg/maps.svg';
+import gears from './svg/gears.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <img src={dolphin} className="My-logo" alt="logo" />
-      <img src={dolphin} className="My-logo" alt="logo" />      
+      <img src={menu} className="My-logo-rev" alt="menu-logo" />
+      <img src={lego} className="My-logo" alt="lego-logo" />
+      <img src={maps} className="My-logo" alt="maps-logo" /> 
+      <img src={gears} className="My-logo" alt="gears-logo" />     
       <header className="App-header">
       <h1>Hello World</h1>
         <img src={logo} className="App-logo" alt="logo" />
