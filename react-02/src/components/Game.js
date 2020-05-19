aaf4beb5912decb9ebe75305f58ac4f3823220db
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-05-19 16:27:49 -0600

    codes defining turn-taking pattern

--- react-02/src/components/Game.js
+++ react-02/src/components/Game.js
@@ -1,30 +1,42 @@
 import React from "react";
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            value: null
-        };
-    }
-    render() {
-        return (
-            <div>
-                <button className="square"
-                    onClick={() => this.setState({ value: "X" })}>
-                    {this.state.value}
-                </button>
-            </div>
-        );
-    }
+function Square(props) {
+    return (
+        <div>
+            <button className="square"
+                onClick={props.onClick}>
+                {props.value}
+            </button>
+        </div>
+    );
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? "X" : "O";
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)} />
+        );
     }
     render() {
-        const status = "Next player: x";
+        const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
         return (
             <div>
                 <div className="status">{status}</div>
