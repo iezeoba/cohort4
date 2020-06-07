1e7a00b3545b6d07d8739d601c14901c4e3a5736
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-06-06 23:57:47 -0600

    created business logic directory and moved in files

--- react-02/src/business/Account.test.js
+++ react-02/src/business/Account.test.js
@@ -27,88 +27,87 @@ test("Testing withdraw", () => {
     expect(myAccount.withdraw(1250)).toBe("Insufficient balance");
 });
 
-// Tests for AccountController methods
+// Tests for AccountController class methods
 test("Testing key", () => {
-    const myCtrl = new AccountController()
-    let key = myCtrl.nextKey();
+    const myAcctController = new AccountController()
+    let key = myAcctController.nextKey();
     expect(key).toBe("k1");
-    key = myCtrl.nextKey();
+    key = myAcctController.nextKey();
     expect(key).toBe("k2");
-    key = myCtrl.nextKey();
+    key = myAcctController.nextKey();
     expect(key).toBe("k3");
 });
 
 test("Testing createAccount", () => {
-    const myCtrl = new AccountController(); //Instantiating an AccountController
+    const myAcctController = new AccountController(); //Instantiating an AccountController
 
     // --- Testing account creation and key assignment --- //
-    expect(myCtrl.bankaccounts.length).toBe(0);
-    let key = myCtrl.createAccount("David Chan", "Savings", 200);
+    expect(myAcctController.bankaccounts.length).toBe(0);
+    let key = myAcctController.createAccount("David Chan", "Savings", 200);
     expect(key).toBe("k1");
-    expect(myCtrl.bankaccounts.length).toBe(1);
-    key = myCtrl.createAccount("David Chan", "Checking", 800);
+    expect(myAcctController.bankaccounts.length).toBe(1);
+    key = myAcctController.createAccount("David Chan", "Checking", 800);
     expect(key).toBe("k2");
-    expect(myCtrl.bankaccounts.length).toBe(2);
-    key = myCtrl.createAccount("David Chan", "RESP", 1200);
+    expect(myAcctController.bankaccounts.length).toBe(2);
+    key = myAcctController.createAccount("David Chan", "RESP", 1200);
     expect(key).toBe("k3");
-    expect(myCtrl.bankaccounts.length).toBe(3);
+    expect(myAcctController.bankaccounts.length).toBe(3);
 
     // --- Testing account retrieval using key --- //
-    let account = myCtrl.getAccount("k1");
+    let account = myAcctController.getAccount("k1");
     expect(account.name).toBe("David Chan");
     expect(account.acctType).toBe("Savings");
     expect(account.key).toBe("k1");
 
-    account = myCtrl.getAccount("k3");
+    account = myAcctController.getAccount("k3");
     expect(account.name).toBe("David Chan");
     expect(account.acctType).toBe("RESP");
     expect(account.key).toBe("k3");
 
     // --- Testing the deposit method using key to retrieve account --- //
-    myCtrl.PerformDeposit("k1", 1);
-    account = myCtrl.getAccount("k1");
+    myAcctController.PerformDeposit("k1", 1);
+    account = myAcctController.getAccount("k1");
     console.log(account);
     expect(account.balance).toBe(201);
 
-    myCtrl.PerformDeposit("k1", 5);
-    account = myCtrl.getAccount("k1");
+    myAcctController.PerformDeposit("k1", 5);
+    account = myAcctController.getAccount("k1");
     console.log(account);
     expect(account.balance).toBe(206);
 
     // --- Testing the withdraw method using key to retrieve account --- //
-    myCtrl.PerformWithdraw("k2", 2);
-    account = myCtrl.getAccount("k2");
+    myAcctController.PerformWithdraw("k2", 2);
+    account = myAcctController.getAccount("k2");
     console.log(account);
     expect(account.balance).toBe(798);
 
-    myCtrl.PerformWithdraw("k2", 8);
-    account = myCtrl.getAccount("k2");
+    myAcctController.PerformWithdraw("k2", 8);
+    account = myAcctController.getAccount("k2");
     console.log(account);
     expect(account.balance).toBe(790);
 
     // --- Testing the delete method using key to retrieve account --- //
-    myCtrl.PerformDelete("k1", myCtrl.bankaccounts);
-    expect(myCtrl.bankaccounts.length).toBe(2);
+    myAcctController.PerformDelete("k1", myAcctController.bankaccounts);
+    expect(myAcctController.bankaccounts.length).toBe(2);
 
-    myCtrl.PerformDelete("k2", myCtrl.bankaccounts);
-    expect(myCtrl.bankaccounts.length).toBe(1);
-    console.log(myCtrl.bankaccounts);
-
+    myAcctController.PerformDelete("k2", myAcctController.bankaccounts);
+    expect(myAcctController.bankaccounts.length).toBe(1);
+    console.log(myAcctController.bankaccounts);
 });
 
-// --- Testing the delete method independently --- //
+// --- Testing the delete method as a new instance --- //
 test("Testing deleteAccount", () => {
-    const myCtrl = new AccountController();
+    const myAcctController = new AccountController();
 
-    let key = myCtrl.createAccount("David Chan", "Checking", 200);
-    key = myCtrl.createAccount("Arnold Smith", "Credit Card", 400);
-    key = myCtrl.createAccount("Michael Keys", "Savings", 600);
+    let key = myAcctController.createAccount("David Chan", "Checking", 200);
+    key = myAcctController.createAccount("Arnold Smith", "Credit Card", 400);
+    key = myAcctController.createAccount("Michael Keys", "Savings", 600);
 
-    expect(myCtrl.bankaccounts.length).toBe(3);
+    expect(myAcctController.bankaccounts.length).toBe(3);
 
-    myCtrl.PerformDelete("k1", myCtrl.bankaccounts);
-    expect(myCtrl.bankaccounts.length).toBe(2);
+    myAcctController.PerformDelete("k1", myAcctController.bankaccounts);
+    expect(myAcctController.bankaccounts.length).toBe(2);
 
-    myCtrl.PerformDelete("k2", myCtrl.bankaccounts);
-    expect(myCtrl.bankaccounts.length).toBe(1);
+    myAcctController.PerformDelete("k2", myAcctController.bankaccounts);
+    expect(myAcctController.bankaccounts.length).toBe(1);
 });
