35ca53ed7725769abbfd41cb7e71cb2e648db069
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-06-09 22:21:09 -0600

    Adding City_Community components and logic files

---
+++ react-02/src/components/City_CommunityAppComponents/CityAppComp.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import AccountGenerator from "./AccountGeneratorComp.js.js"
+import TransactBar from "./TransactBarComp.js.js"
+import AccountCard from './AccountCardComp.js.js';
+import { Account } from '../../business/Account.js'
+
+class CityApp extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            acctDetail: [],
+            acctName: "",
+            acctType: "",
+            idTrxFrom: "",
+            idTrxAmount: ""
+        }
+        this.newAccount = new Account();
+    }
+
+    handleCreateAcct = () => {
+        console.log("creating account", this.state.acctName, this.state.acctType);
+        // let key = myCtrl.createAccount("David Chan", "Savings", 200);
+
+        // if (this.newAccount.accountTypeExists(this.state.acctType)) {
+        //     return
+        // }
+        // else if (this.newAccount.accountTypeExists(this.state.acctType) === false) {
+        //     this.newAccount.createAccount(this.state.acctName, this.state.acctType)
+        //     this.setState({
+        //         acctDetail: this.newAccount.allAccounts
+        //     })
+        //     console.log(this.newAccount.allAccounts);
+        // } else {
+        //     this.newAccount.createAccount(this.state.acctName, this.state.acctType)
+        //     this.setState({
+        //         acctDetail: this.newAccount.allAccounts
+        //     })
+        //     console.log(this.newAccount.allAccounts);
+        // }
+    }
+
+    handleAcctName = (e) => {
+        this.setState({
+            acctName: e.target.value
+        })
+    }
+
+    handleAcctType = (e) => {
+        this.setState({
+            acctType: e.target.value
+        })
+    }
+
+    handleTrxFrom = (e) => {
+        this.setState({
+            idTrxFrom: e.target.value
+        })
+    }
+
+    handleTrxAmount = (e) => {
+        this.setState({
+            idTrxAmount: e.target.value
+        })
+    }
+
+    // handleDeposit(account) {
+    //     if (account === this.state.idTrxFrom) {
+    //         this.newAccount.idTrxFrom.deposit(this.state.idTrxAmount)
+    //         this.setState({
+    //             acctDetail: this.newAccount.allAccounts
+    //         })
+    //     }
+    // }
+
+    handleDeposit = () => {
+        if (this.newAccount.accountTypeExists(this.state.idTrxFrom)) {
+            this.newAccount.deposit(this.state.idTrxAmount)
+            this.setState({
+                acctDetail: this.newAccount.allAccounts
+            })
+            console.log(this.newAccount.allAccounts);
+        }
+    }
+    handleWithdraw() {
+
+    }
+
+    render() {
+        return (
+            <div className="clAcctContainer">
+                <AccountGenerator handleCreateAcct={this.handleCreateAcct} acctNameChange={this.handleAcctName} acctTypeChange={this.handleAcctType} />
+                <TransactBar trxFromSelected={this.handleTrxFrom} TrxAmountInput={this.handleTrxAmount} handleDeposit={this.handleDeposit} handleWithdraw={this.handleWithdraw} />
+                <AccountCard details={this.state.acctDetail} />
+            </div>
+        );
+    }
+}
+export default BankApp
