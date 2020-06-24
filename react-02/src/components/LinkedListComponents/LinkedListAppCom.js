a07c773a1816c677785560d8a28d5468be425017
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-06-24 13:29:13 -0600

    added linkedlist app component

---
+++ react-02/src/components/LinkedListComponents/LinkedListAppCom.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './LinkedList.css';
+
+class LinkedListApp extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: ""
+        }
+    }
+
+    render() {
+        return (
+            <div className="clLinkedListContainer">
+
+            </div>
+        );
+    }
+}
+export default LinkedListApp
