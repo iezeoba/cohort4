0d6cc0b911d42325dc89eff5affaa0ef29296888
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-06-25 23:52:56 -0600

    added new component to handle theme toggling

---
+++ react-02/src/components/ContextAppComponents/ThemeToggleComp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ThemeContext } from '../../contexts/ThemeContext';
+
+class ThemeToggle extends React.Component {
+    static contextType = ThemeContext;
+    render() {
+        const { toggleTheme } = this.context;
+        return (
+            <button onClick={toggleTheme}>Toggle Theme</button>
+        )
+    }
+}
+export default ThemeToggle;
