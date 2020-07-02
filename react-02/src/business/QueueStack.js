50dc788fa22544e75905e479dff095041be28377
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-07-02 00:41:31 -0600

    enhanced business logic codes

--- react-02/src/business/QueueStack.js
+++ react-02/src/business/QueueStack.js
@@ -2,22 +2,27 @@ class Stack {
     constructor() {
         this.storage = {};
         this.size = 0;
+        this.allStackElements = [];
     }
 
     push(element) {
         this.size++;
         this.storage[this.size] = element;
+        this.allStackElements.push(element) //To store all stacked elements in an array
     }
 
     pop() {
         let removed = this.storage[this.size]
         delete this.storage[this.size]
         this.size--
+        this.allStackElements.pop();
         return removed
     }
 
     peek() {
+        console.log(this.storage[this.size]);
         return this.storage[this.size]
+
     }
 }
 
@@ -26,17 +31,20 @@ class Queue {
         this.storage = {};
         this.head = 0;
         this.tail = 0;
+        this.allQueueElements = [];
     }
 
     enqueue(element) {
         this.storage[this.tail] = element
-        this.tail++
+        this.tail++;
+        this.allQueueElements.push(element);    //To store all queued elements in an array
     }
 
     dequeue() {
         let removed = this.storage[this.head]
         delete this.storage[this.head]
         this.head++
+        this.allQueueElements.shift();  //To manipulate all queued elements in an array
         return removed
     }
 }
