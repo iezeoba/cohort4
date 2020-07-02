d77f76e60bebf5cfa2003e4484c8fe87d7807c33
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-07-02 00:47:04 -0600

    added new SPA to render

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -6,6 +6,8 @@ import Navbar from './components/Navbar';
 import BankAppComp from './components/BankAppComponents/BankAppComp';
 import CityAppComp from './components/City_CommunityAppComponents/CityAppComp';
 import LinkedListAppComp from './components/LinkedListComponents/LinkedListAppCom';
+import QueueStackAppComp from './components/QueueStackComponents/QueueStackAppComp';
+import ContextAppComp from './components/ContextAppComponents/ContextAppComp.js';
 
 // function App() {
 class App extends React.Component {
@@ -22,6 +24,9 @@ class App extends React.Component {
     let idAccount = document.getElementById("account");
     let idCity = document.getElementById("city");
     let idLinkedList = document.getElementById("linkedList");
+    let idQueueStack = document.getElementById("queueStack");
+    let idContext = document.getElementById("context");
+
     if (event.target.id === "id-reactlogo") {
       this.setState({
         value: "This is the react icon"
@@ -31,6 +36,8 @@ class App extends React.Component {
       idAccount.style.display = "none";
       idCity.style.display = "none";
       idLinkedList.style.display = "none";
+      idQueueStack.style.display = "none";
+      idContext.style.display = "none";
     }
     else if (event.target.id === "id-lego") {
       this.setState({
@@ -41,6 +48,8 @@ class App extends React.Component {
       idAccount.style.display = "none";
       idCity.style.display = "none";
       idLinkedList.style.display = "none";
+      idQueueStack.style.display = "none";
+      idContext.style.display = "none";
     }
     else if (event.target.id === "id-menu") {
       this.setState({
@@ -51,6 +60,8 @@ class App extends React.Component {
       idAccount.style.display = "block";
       idCity.style.display = "none";
       idLinkedList.style.display = "none";
+      idQueueStack.style.display = "none";
+      idContext.style.display = "none";
     }
     else if (event.target.id === "id-maps") {
       this.setState({
@@ -61,16 +72,44 @@ class App extends React.Component {
       idAccount.style.display = "none";
       idCity.style.display = "block";
       idLinkedList.style.display = "none";
+      idQueueStack.style.display = "none";
+      idContext.style.display = "none";
     }
-    else if (event.target.id === "id-gears") {
+    else if (event.target.id === "id-network") {
       this.setState({
-        value: "This is the settings icon"
+        value: "This is the network-linkedlist icon"
       });
       idHome.style.display = "none";
       idGame.style.display = "none";
       idAccount.style.display = "none";
       idCity.style.display = "none";
       idLinkedList.style.display = "block";
+      idQueueStack.style.display = "none";
+      idContext.style.display = "none";
+    }
+    else if (event.target.id === "id-layers") {
+      this.setState({
+        value: "This is the layers-queue-stack icon"
+      });
+      idHome.style.display = "none";
+      idGame.style.display = "none";
+      idAccount.style.display = "none";
+      idCity.style.display = "none";
+      idLinkedList.style.display = "none";
+      idQueueStack.style.display = "block";
+      idContext.style.display = "none";
+    }
+    else if (event.target.id === "id-gears") {
+      this.setState({
+        value: "This is the react context icon"
+      });
+      idHome.style.display = "none";
+      idGame.style.display = "none";
+      idAccount.style.display = "none";
+      idCity.style.display = "none";
+      idLinkedList.style.display = "none";
+      idQueueStack.style.display = "none";
+      idContext.style.display = "block";
     }
   }
 
@@ -94,6 +133,12 @@ class App extends React.Component {
         </div>
         <div id="linkedList" style={{ display: "none" }}>
           <LinkedListAppComp />
+        </div>
+        <div id="queueStack" style={{ display: "none" }}>
+          <QueueStackAppComp />
+        </div>
+        <div id="context" style={{ display: "none" }}>
+          <ContextAppComp />
         </div>
       </div>
     );
