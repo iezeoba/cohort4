66d3a7b4fb1405264ab284b2bf18c1447bbc6248
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-07-02 00:43:06 -0600

    refactored codes

--- react-02/src/components/ContextAppComponents/ContextAppComp.js
+++ react-02/src/components/ContextAppComponents/ContextAppComp.js
@@ -1,21 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import NavBar from './NavBarComp.js';
 import BookList from './BookListComp.js';
 import ThemeContextProvider from '../../contexts/ThemeContext.js';
 import ThemeToggle from './ThemeToggleComp.js';
 import AuthContextProvider from '../../contexts/AuthContext.js';
+import Heading from './HeadingComp.js';
 import './ContextApp.css';
 
 function ContextApp() {
     return (
         <div className="clContextAppContainer">
-            <ThemeContextProvider>
-                <AuthContextProvider>
-                    <NavBar />
-                    <BookList />
-                    <ThemeToggle />
-                </AuthContextProvider>
-            </ThemeContextProvider>
+            <h1>Context App</h1>
+            <div id="idContextAppContainer">
+                <Heading />
+                <ThemeContextProvider>
+                    <AuthContextProvider>
+                        <NavBar />
+                        <BookList />
+                        <ThemeToggle />
+                    </AuthContextProvider>
+                </ThemeContextProvider>
+            </div>
         </div>
     );
 };
